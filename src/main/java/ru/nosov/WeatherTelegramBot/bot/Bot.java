00a0c71b2ba6ff6c1cf23157a3f786625890c9c0
Игорь Носов <NosovID@gmail.com>
2021-09-29 18:23:19 +0300

    add Hello response

--- src/main/java/ru/nosov/WeatherTelegramBot/bot/Bot.java
+++ src/main/java/ru/nosov/WeatherTelegramBot/bot/Bot.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.nosov.WeatherTelegramBot.config.BotConfig;
 
 @Component
@@ -29,8 +31,23 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        log.debug("Receive new Update. updateID: " + update.getUpdateId());
 
+        Long chatId = update.getMessage().getChatId();
+        String inputText = update.getMessage().getText();
+
+        if (inputText.startsWith("/start")) {
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId.toString());
+            message.setText("Hello. This is start message");
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 
     @Override
     public String getBotUsername() {
--- src/main/java/ru/nosov/WeatherTelegramBot/config/BotConfig.java
+++ src/main/java/ru/nosov/WeatherTelegramBot/config/BotConfig.java
@@ -3,7 +3,6 @@ package ru.nosov.WeatherTelegramBot.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Data
